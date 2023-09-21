14813c61bbb7c1e32915bc8caa22e222d6a3695a
Gabriel Bernardo Meireles <gbmreistom@gmail.com>
2023-09-21 11:52:52 -0300

    adding database

--- db/db.go
+++ db/db.go
@@ -1,1 +1,30 @@
 package db
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+
+	"github.com/jackc/pgx/v5"
+)
+
+var db *pgx.Conn
+
+func GetDatabase() *pgx.Conn {
+	if db == nil {
+		Connect()
+	}
+	return db
+}
+
+func Connect() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	db = conn
+}
